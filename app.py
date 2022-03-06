234d01dc521ea3614bcf6fa1400930731a3ba8ff
Salmanmohds <salmangit83@gmail.com>
2022-03-06 14:13:48 +0530

    Commtted for Bug Fix

--- app.py
+++ app.py
@@ -156,10 +156,11 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
-from .database.db import initialize_db
+# from .database.db import initialize_db
+from database.db import initialize_db
 from flask_restful import Api
-from .view.routes import initialize_routes
-from .view.errors import errors
+from view.routes import initialize_routes
+from view.errors import errors
 
 app = Flask(__name__)
 api = Api(app, errors=errors)
